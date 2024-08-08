fe11c064d056cddf8fba8fa28798115e26a86513
Arnold <faiarnold@gmail.com>
2024-08-08 16:02:44 +0100

    Without the use of regular expressions

---
+++ passgen.py
@@ -0,0 +1,19 @@
+import random
+import string
+UC= string.ascii_uppercase
+LC= string.ascii_lowercase
+Dig= string.digits
+SP= "!@#$%^&*_+-/|"
+all= UC + LC + Dig + SP
+
+def generate_password(length):
+    if length< 8:
+        raise ValueError("number length invalid")
+
+    password = [random.choice(UC), random.choice(LC), random.choice(Dig), random.choice(SP)]
+    password += random.choices(all, k= length - 8)
+    random.shuffle(password)
+    return ''.join(password)
+password_length = int(input())
+password= generate_password(password_length)
+print(password)
